e22ac79b1abd1b6bfa7aaffb0adaddf83d1737cb
maya-malaviya <maya.anu.malaviya@gmail.com>
2026-07-29 22:46:16 -0400

    reordering main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ class Index extends Component {
       return (
         <Layout>
               <div className = "page" style={{zIndex: "-1 !important"}}>
-                  <div className = "title" style={{color: "black", height: "130vh", marginBottom:"-30vh"}} />
+                  <div className = "title" style={{color: "black", height: "95vh", marginBottom:"-30vh"}} />
                   <div className = "namecard">
                       <div className="desktop-only">
                         <h1>Growing<br/>up in<br/>Science</h1>
@@ -27,8 +27,8 @@ class Index extends Component {
                         </h2>
                       </div>
                     </div>
-                  <RecentEvents/>
                   <About/>
+                  <RecentEvents/>
               </div>
         </Layout>
       );
